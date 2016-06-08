967e6dfecaa0b7482fa911ff7aafed6f8a5f5d72
gaoyuanhezhihao <gaoyuanhezhihao@139.com>
2016-06-08 22:01:02 +0800

    pca is ok

--- Simple_EigenFace/main.cpp
+++ Simple_EigenFace/main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 using namespace cv;
 
 bool concatenate_image_row(vector<Mat> & image_vector, Mat& train_imags, Size& im_size);
+Mat norm_0_255(const Mat& src);
 Size calc_new_size(vector<Mat>::iterator & it_ref, int rows_max, int cols_max);
 
 int main(int argc, char ** argv) {
@@ -33,10 +34,34 @@ int main(int argc, char ** argv) {
 		train_im_vec.push_back(imread(image_dir + '\\' + im_id + ".jpg", IMREAD_GRAYSCALE));
 	}
 	concatenate_image_row(train_im_vec, train_imgs, im_size);
+	// pca
+	// Number of components to keep for the PCA:
+	int num_components = 10;
+
+	// Perform a PCA:
+	PCA pca(train_imgs, Mat(), CV_PCA_DATA_AS_ROW, num_components);
+
+	// And copy the PCA results:
+	Mat mean = pca.mean.clone();
+	Mat eigenvalues = pca.eigenvalues.clone();
+	Mat eigenvectors = pca.eigenvectors.clone();
+
+	// The mean face:
+	imwrite("avg.jpg", norm_0_255(mean.reshape(1, im_size.height)));
+
+	// The first three eigenfaces:
+	imwrite("pc1.jpg", norm_0_255(pca.eigenvectors.row(0)).reshape(1, im_size.height));
+	imwrite("pc2.jpg", norm_0_255(pca.eigenvectors.row(1)).reshape(1, im_size.height));
+	imwrite("pc3.jpg", norm_0_255(pca.eigenvectors.row(2)).reshape(1, im_size.height));
+
+	// Show the images:
+	waitKey(0);
+
+	//debug
 	im = imread(image_dir + '\\' + to_string(11) + ".jpg", IMREAD_GRAYSCALE);
-	imshow("original image", im);
+	imwrite("original image.jpg", im);
 	Mat rebuild_im = train_imgs.row(11-1).clone();
-	imshow("rebuild image", rebuild_im.reshape(0, im_size.height));
+	imwrite("rebuild image.jpg", rebuild_im.reshape(0, im_size.height));
 	waitKey(0);
 	return 0;
 }
@@ -62,14 +87,19 @@ bool concatenate_image_row(vector<Mat> & image_vector, Mat& train_imags, Size& i
 	im_size.height = rows_max;
 	// resize every image to the max size and concatenate them.
 	vector<Mat>::iterator it = image_vector.begin();
-	Mat im_resized=Mat::zeros(rows_max,column_max,image_vector[0].type());
+	Mat im_std_size=Mat::zeros(rows_max,column_max,CV_64FC1);
+	Mat im_resized;
 	Size new_size;
 	for (; it != image_vector.end(); ++it) {
-		new_size = calc_new_size(it, rows_max, column_max);
-		im_resized = Mat::zeros(rows_max, column_max, image_vector[0].type());
-		resize(*it, im_resized(Rect(0, 0, new_size.width, new_size.height)), new_size);
+		//new_size = calc_new_size(it, rows_max, column_max);
+		//resize(*it, im_resized, new_size);
+		//im_resized.copyTo(im_std_size(Rect(0, 0, new_size.width, new_size.height)));
+		im_std_size = Mat::zeros(rows_max, column_max, CV_64FC1);
+		resize(*it, im_resized, Size(column_max, rows_max));
+		im_resized.copyTo(im_std_size(Rect(0, 0, column_max, rows_max)));
+
 		//it->copyTo(im_resized(Rect(0, 0, it->cols, it->rows)));
-		train_imags.push_back(im_resized.reshape(0, 1));
+		train_imags.push_back(im_std_size.reshape(0, 1));
 	}
 	return true;
 }
@@ -89,3 +119,20 @@ Size calc_new_size(vector<Mat>::iterator & it_ref, int rows_max, int cols_max) {
 	return new_size;
 }
 
+// Normalizes a given image into a value range between 0 and 255.
+Mat norm_0_255(const Mat& src) {
+	// Create and return normalized image:
+	Mat dst;
+	switch (src.channels()) {
+	case 1:
+		cv::normalize(src, dst, 0, 255, NORM_MINMAX, CV_8UC1);
+		break;
+	case 3:
+		cv::normalize(src, dst, 0, 255, NORM_MINMAX, CV_8UC3);
+		break;
+	default:
+		src.copyTo(dst);
+		break;
+	}
+	return dst;
+}
